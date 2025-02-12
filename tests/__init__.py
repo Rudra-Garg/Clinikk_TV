52bda2a1e4d3c619d68b82ea6196295ef7926cd1
Rudra Garg <rudra.garg22b@iiitg.ac.in>
2025-02-12 15:02:03 +0530

    Add Tests

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+"""
+This package contains all the test modules for the Clinikk TV Backend application.
+Tests cover authentication, content operations, health checks, and other critical functions.
+"""
